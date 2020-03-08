7e0cc63d63354fdd3490852d7e668d83ab4271fe
song <1007761397@qq.com>
2020-03-09 01:23:25 +0800

    重新创建

--- eureka/src/test/java/com/imooc/eureka/EurekaApplicationTests.java
+++ eureka/src/test/java/com/imooc/eureka/EurekaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.imooc.client;
+package com.imooc.eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ClientApplicationTests {
+class EurekaApplicationTests {
 
     @Test
     void contextLoads() {
--- eureka2/src/test/java/com/imooc/eureka2/Eureka2ApplicationTests.java
+++ eureka2/src/test/java/com/imooc/eureka2/Eureka2ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.imooc.client;
+package com.imooc.eureka2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ClientApplicationTests {
+class Eureka2ApplicationTests {
 
     @Test
     void contextLoads() {
